6fc57832bdcad8cfa85999e50e09126155aa85f8
WolfGames156 <ktme156@gmail.com>
2025-06-25 14:17:52 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Minecraft sunucusunu arka planda başlat
-java -Xmx1G -Xms1G -jar paper.jar nogui &
+java -Xmx1G -Xms1G -jar paper-1.16.5-792.jar nogui &
 
 # playit.gg tünelini başlat (ilk seferde link kodu verir)
 ./playit
